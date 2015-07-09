49478640223bd2c321e55a351444001bee8b1922
Skyler Clark <skyler@whitecapinstitute.com>
2015-07-09 10:54:52 -0600

    first commit

---
+++ UINavigationController/Navigation.h
@@ -0,0 +1,13 @@
+//
+//  Navigation.h
+//  UINavigationController
+//
+//  Created by Skyler Clark on 7/9/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Navigation : UINavigationController
+
+@end
---
+++ UINavigationController/RedViewController.h
@@ -0,0 +1,13 @@
+//
+//  RedViewController.h
+//  UINavigationController
+//
+//  Created by Skyler Clark on 7/9/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RedViewController : UIViewController
+
+@end
